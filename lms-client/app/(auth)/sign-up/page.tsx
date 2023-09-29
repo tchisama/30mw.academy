1c31222318f3ff1b0434c3d98d82ea0221c2c094
tchi-sama <tchisama41@gmail.com>
2023-09-28 21:45:30 -0700

    sign in and sign up is done now

--- lms-client/app/(auth)/sign-up/page.tsx
+++ lms-client/app/(auth)/sign-up/page.tsx
@@ -8,10 +8,10 @@ import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card"
-import illu from "@/public/login1.png"
+import illu from "@/public/signup1.png"
 import Image from "next/image"
 import Link from "next/link"
-import LoginComponent from "@/components/global/Login-component"
+import SignUpComponent from "@/components/global/Sign-up-component"
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -31,18 +31,23 @@ export default function UserAuthForm({ className, ...props }: UserAuthFormProps)
     <div className="grid grid-cols-2 min-h-screen justify-center items-center">
       <div className="w-full h-full relative bg-primary justify-center items-center flex">
           <Image alt="illustration" className="w-[70%] object-contain drop-shadow-xl" src={illu}></Image>
+          <Link className="absolute bottom-4 right-6 text-white" href="/">
+            Create an teacher accout
+          </Link>
       </div>
-      <Link href="/sign-up" className="absolute top-6 right-8">
-        <Button>Sign up</Button>
+      <Link href="/sign-in" className="absolute top-6 right-8">
+        <Button>Sign in</Button>
       </Link>
 
       <Link href="/" className="absolute top-10 left-10">
         <h1 className="text-white text-3xl font-bold">LMS</h1>
       </Link>
 
-      <div className="w-[400px] mx-auto">
-        <LoginComponent />
-      </div>
+        <div className="w-[400px] mx-auto">
+            <SignUpComponent/>
+        </div>
+
+
     </div>
   )
 }
---
+++ lms-client/components/global/Login-component.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { Icons } from "@/components/icons"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "../ui/button"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { Facebook, FacebookIcon } from "lucide-react"
+
+export default function LoginComponent() {
+  return (
+    <Card className="shadow-xl">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">Account login</CardTitle>
+        <CardDescription>
+          Enter your email below to login.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid grid-cols-2 gap-6">
+          <Button variant="outline">
+            <Facebook className="mr-2 fill-foreground h-5 w-5" strokeWidth={0}/>
+            Facebook
+          </Button>
+          <Button variant="outline">
+            <Icons.google className="mr-2 h-4 w-4" />
+            Google
+          </Button>
+        </div>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="email">Full name</Label>
+          <Input id="email" type="email" placeholder="your name" />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Create account</Button>
+      </CardFooter>
+    </Card>
+  )
+}
