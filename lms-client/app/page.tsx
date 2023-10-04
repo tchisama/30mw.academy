e7888ddb594556675478c71600c2cfe34c871dd2
tchi-sama <tchisama41@gmail.com>
2023-10-04 08:51:45 -0700

    fetch config and desplay

--- lms-client/app/page.tsx
+++ lms-client/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Navbar from "@/components/global/Navbar"
-import { ArrowRight } from "lucide-react"
+import { ArrowRight, Loader } from "lucide-react"
 import _30mw from "@/public/30mw.png"
 import _s3d from "@/public/s3d.png"
 import Image from "next/image"
@@ -10,27 +10,48 @@ import { useClerk } from "@clerk/nextjs"
 import TypeWriter from "@/components/global/TypingWriter"
 import Link from "next/link"
 
-
+interface LandingPage {
+  landing_page: {
+    Header: string;
+    Text: string;
+    learn: string[];
+    button: string;
+  };
+  _id: string;
+  __v: number;
+}
 
 export default function Home() {
   const user = useClerk()
-  const [config, setConfig] = useState({})
-  
-
+  const [config, setConfig] = useState<LandingPage>()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch('http://localhost:8080/config')
+      .then(res => res.json())
+      .then(data =>{
+          setConfig(data)
+          setLoading(false)
+      })
+  },[])
+  if(loading){
+    return <div className='h-screen flex justify-center items-center '>
+                <h1 className='flex gap-3'><Loader className='animate-spin'/>Loading...</h1>
+            </div>
+  }
   return (
       <div className="w-full   container mx-auto">
         <div className="py-4 flex min-h-screen flex-col">
             <Navbar/>
           <div className="my-8 pt-24 flex-1 flex flex-col gap-2 justify-center items-center">
             <h1 className="text-7xl text-center font-bold ">
-                Welcome to 30MW Academy🎉 
+              {config?.landing_page?.Header}
             </h1>
             <h2 className="text-3xl text-mutid-foreground text-center">
-                Your Learning Hub. Unlock new horizons today.
+              {config?.landing_page?.Text}
             </h2>
             <h2 className="text-4xl font-medium text-mutid-foreground text-center">
                Learn {" "} 
-              <TypeWriter data={["Design 🎨", "Video Editing 🎥", "3d Modelling 🏡"]}></TypeWriter>
+              <TypeWriter data={config?.landing_page?.learn||[]}></TypeWriter>
             </h2>
             <div className="p-8">
               <Link href="/courses" className="px-8 shadow-2xl hover:scale-105 items-center duration-150 py-4 text-lg bg-primary text-white uppercase rounded-full flex gap-2 ">get started <ArrowRight/></Link>
