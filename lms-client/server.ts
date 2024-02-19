ae465e6d534b9e739b2567449bb7155be4760b72
tchisama <tchisama41@gmail.com>
2024-02-19 08:28:14 +0100

    sfg

--- lms-client/server.ts
+++ lms-client/server.ts
@@ -1,4 +1,4 @@
-export const dev = true
+export const dev = false
 
 export const server =dev?"http://localhost:8080/":"https://three0mw-academy.onrender.com/"
 // "https://clean-ant-hoodie.cyclic.app/"
