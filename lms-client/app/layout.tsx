17505d8af151183cc809e73fdd025f3aaab1e24d
tchisama <tchisama41@gmail.com>
2024-03-20 13:33:03 +0000

    add google tag manager and try to fix the building error

--- lms-client/app/layout.tsx
+++ lms-client/app/layout.tsx
@@ -1,65 +1,67 @@
-import { ThemeProvider } from '@/components/theme-provider'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Rubik } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { EdgeStoreProvider } from '@/lib/edgestore'
-import CatsProvider from '@/components/CatsProvider'
-import { dev } from '@/server'
-import { Code } from 'lucide-react'
-import Navbar from '@/components/global/Navbar'
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+import CatsProvider from "@/components/CatsProvider";
+import { dev } from "@/server";
+import { Code } from "lucide-react";
+import Navbar from "@/components/global/Navbar";
+import { GoogleTagManager } from "@next/third-parties/google";
 
-const inter = Rubik({ subsets: ['arabic'] })
+const inter = Rubik({ subsets: ["arabic"] });
 
 export const metadata: Metadata = {
-  title: '30MW Academy',
-  description: '',
-}
+  title: "30MW Academy",
+  description: "",
+};
 
 export default function RootLayout({
-
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-
-
-
-  
   return (
     <ClerkProvider>
       <html lang="en">
         <head>
-          <link rel='icon' href="/favicon.png" sizes={"any"} />
+          <link rel="icon" href="/favicon.png" sizes={"any"} />
         </head>
 
+        {/* adding the google tag manager tag to targit all the pages */}
+
+        <GoogleTagManager gtmId="AW-16503732124" />
+        
+
+
+
+
         <body className={inter.className}>
-          {
-            dev&&
-        <div className="absolute flex gap-2 top-2 left-2 bg-primary text-white p-2 rounded-md w-fit">
-           <Code/> Mode
-        </div>
-          }
+          {dev && (
+            <div className="absolute flex gap-2 top-2 left-2 bg-primary text-white p-2 rounded-md w-fit">
+              <Code /> Mode
+            </div>
+          )}
           <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-              >
-                <EdgeStoreProvider>
-                  <CatsProvider>
-                    <div className='min-h-screen  flex flex-col'>
-                      <div className='max-w-[1800px] px-4 w-full mx-auto'>
-                        <Navbar />
-                      </div>
-                      {children}
-                    </div>
-                  </CatsProvider>
-                </EdgeStoreProvider>
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <EdgeStoreProvider>
+              <CatsProvider>
+                <div className="min-h-screen  flex flex-col">
+                  <div className="max-w-[1800px] px-4 w-full mx-auto">
+                    <Navbar />
+                  </div>
+                  {children}
+                </div>
+              </CatsProvider>
+            </EdgeStoreProvider>
           </ThemeProvider>
         </body>
       </html>
-      </ClerkProvider>
-  )
+    </ClerkProvider>
+  );
 }
