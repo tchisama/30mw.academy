eef6e0839d6b6cbf308dc252739d42e11baf8504
tchisama <tchisama41@gmail.com>
2024-03-20 12:19:30 +0000

    remove response image just till i fix it

--- lms-client/app/api/seo/og/route.tsx
+++ lms-client/app/api/seo/og/route.tsx
@@ -1,15 +1,13 @@
-import axios from "axios";
-import { ImageResponse, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET(request: Request) {
   try {
-    const { searchParams } = new URL(request.url);
-    return new ImageResponse(
-      <img src={"https://files.edgestore.dev/o5czfp70y8ctkrhw/publicFiles/_public/d9fb41bc-7a4b-44d2-90d1-aa1bedceaa86.jpg"}></img>
-    )
+    // const { searchParams } = new URL(request.url);
+    return NextResponse.json({ message: "still not ready yet"});
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "Failed to generate the image" });
   }
 }
+
