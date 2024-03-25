1b7bc6a79e9abf08cd36e2accc003df040570274
tchisama <tchisama41@gmail.com>
2024-03-25 09:47:42 +0000

    add

--- lms-client/app/api/(30mw-server)/course/route.ts
+++ lms-client/app/api/(30mw-server)/course/route.ts
@@ -1,32 +1,27 @@
-
 import dbConnect from "@/lib/db";
 import { NextResponse } from "next/server";
 import CourseModel from "@/models/Course";
 
-
-
 export async function POST(request: Request) {
+  await dbConnect();
   try {
-    console.log("fire route")
+    console.log("fire route");
     const newCourse = new CourseModel(await request.json());
     const savedCourse = await newCourse.save();
-    return NextResponse.json({id:savedCourse._id});
+    return NextResponse.json({ id: savedCourse._id });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Failed to create the course' });
+    return NextResponse.json({ message: "Failed to create the course" });
   }
 }
-
 
 export async function DELETE(request: Request) {
   try {
     const { id } = await request.json();
     await CourseModel.findByIdAndDelete(id);
-    return NextResponse.json({ message: 'Course deleted successfully' });
+    return NextResponse.json({ message: "Course deleted successfully" });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Failed to delete the course' });
+    return NextResponse.json({ message: "Failed to delete the course" });
   }
 }
-
-
