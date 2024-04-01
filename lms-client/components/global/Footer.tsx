a8c17f8f83d730e46bc6246e0a884c93fe20892e
tchisama <pro.tchisama@gmail.com>
2024-04-01 05:51:09 +0000

    fix footer dark mode and add youtube link

--- lms-client/components/global/Footer.tsx
+++ lms-client/components/global/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram  } from 'lucide-react';
+import { Facebook, Instagram, YoutubeIcon  } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
@@ -6,17 +6,22 @@ import { FaWhatsapp } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="min-h-[100px] py-8 flex items-center justify-center flex-col md:flex-row gap-4">
-      <Link className="p-3 bg-white flex gap-3 items-center border rounded-xl " href="https://www.instagram.com/30mwacademy">
+      <Link className="p-3  flex gap-3 items-center md:border rounded-xl " href="https://www.instagram.com/30mwacademy">
         <Instagram strokeWidth={2} size={28} className="text-3xl" />
         30mwacademy
       </Link>
-      <Link className="p-3 bg-white  flex gap-3 items-center border rounded-xl " href="https://www.facebook.com/30mwacademy">
+      <Link className="p-3   flex gap-3 items-center md:border rounded-xl " href="https://www.facebook.com/30mwacademy">
         <Facebook strokeWidth={2} size={28} className="text-3xl" />
         30mwacademy
       </Link>
-      <Link className="p-3 bg-white flex gap-3 items-center border rounded-xl " href="https://wa.me/212642680949">
+      <Link className="p-3  flex gap-3 items-center md:border rounded-xl " href="https://wa.me/212642680949">
         <FaWhatsapp strokeWidth={1} size={28} className="text-3xl" />
         0642680949
+      </Link>
+      <Link className="p-3  flex gap-3 items-center md:border rounded-xl " href="https://youtube.com/@abdeoqba">
+        <YoutubeIcon
+          strokeWidth={2} size={28} className="text-3xl" />
+        30mwacademy
       </Link>
     </footer>
   );
