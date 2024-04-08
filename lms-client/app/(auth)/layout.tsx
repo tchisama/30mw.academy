f290f770a23b45760b5efcd491b676867c8b81ba
tchisama <pro.tchisama@gmail.com>
2024-04-08 14:28:50 +0000

    added facebook pixel as well

--- lms-client/app/(auth)/layout.tsx
+++ lms-client/app/(auth)/layout.tsx
@@ -1,8 +1,12 @@
+import { FacebookPixelEvents } from '@/components/global/FacebookPixel'
 import React from 'react'
 
 function layout({children}:{children: React.ReactNode}) {
   return (
-        <div>{children}</div>
+    <div>
+      {children}
+      <FacebookPixelEvents/>
+    </div>
   )
 }
 
