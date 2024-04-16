3802d6c3d5c310ea0b9f5a64cd3aa5bdf5b8576c
tchisama <pro.tchisama@gmail.com>
2024-04-16 20:58:30 +0100

    fix error of home page without titles

--- lms-client/app/api/(30mw-server)/config/route.ts
+++ lms-client/app/api/(30mw-server)/config/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import ConfigModel from "@/models/Config.js";
+import db from "@/lib/db";
 
 export async function GET(request: Request) {
+  await db();
   try {
     const config = await ConfigModel.findOne();
     return NextResponse.json(config);
